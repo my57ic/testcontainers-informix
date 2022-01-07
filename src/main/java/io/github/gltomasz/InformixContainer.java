faf575b7ab705fde7317e22a700126e9b1982b53
Sualeh Fatehi <sualeh@hotmail.com>
2022-01-07 15:40:58 -0500

    Build on Java 8 (compatible with Testcontainers) and with updated depdencies

--- src/main/java/io/github/gltomasz/InformixContainer.java
+++ src/main/java/io/github/gltomasz/InformixContainer.java
@@ -1,102 +1,109 @@
 package io.github.gltomasz;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.time.Duration;
+
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
-import java.nio.file.Path;
-import java.text.MessageFormat;
-import java.time.Duration;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
-
 class InformixContainer<SELF extends InformixContainer<SELF>> extends JdbcDatabaseContainer<SELF> {
 
-    public static final int INFORMIX_PORT = 9088;
-    static final String DEFAULT_USER = "informix";
-    static final String DEFAULT_PASSWORD = "in4mix";
-    private static final String IFX_CONFIG_DIR = "/opt/ibm/config/";
-    private String databaseName = "sysadmin";
+  private enum FileType {
+    INIT_FILE,
+    RUN_FILE_POST_INIT
+  }
 
-    public InformixContainer() {
-        this(DockerImageName.parse(InformixContainerProvider.FULL_IMAGE_NAME + ":latest"));
-    }
+  public static final int INFORMIX_PORT = 9088;
+  static final String DEFAULT_USER = "informix";
+  static final String DEFAULT_PASSWORD = "in4mix";
+  private static final String IFX_CONFIG_DIR = "/opt/ibm/config/";
 
-    public InformixContainer(DockerImageName dockerImageName) {
-        super(dockerImageName);
-        this.waitStrategy = new LogMessageWaitStrategy()
-                .withRegEx(".*Maximum server connections 1.*")
-                .withTimes(1)
-                .withStartupTimeout(Duration.of(60, SECONDS));
-        addExposedPort(INFORMIX_PORT);
-    }
+  private String databaseName = "sysadmin";
 
-    @Override
-    protected void configure() {
-        addEnv("LICENSE", "accept");
-    }
+  public InformixContainer() {
+    this(DockerImageName.parse(InformixContainerProvider.FULL_IMAGE_NAME + ":latest"));
+  }
 
-    @Override
-    public String getDriverClassName() {
-        return "com.informix.jdbc.IfxDriver";
-    }
+  public InformixContainer(final DockerImageName dockerImageName) {
+    super(dockerImageName);
+    this.waitStrategy =
+        new LogMessageWaitStrategy()
+            .withRegEx(".*Maximum server connections 1.*")
+            .withTimes(1)
+            .withStartupTimeout(Duration.of(60, SECONDS));
+    addExposedPort(INFORMIX_PORT);
+  }
 
-    @Override
-    public String getJdbcUrl() {
-        String additionalUrlParams = constructUrlParameters(";", ";");
-        return MessageFormat.format("jdbc:informix-sqli://{0}:{1}/{2}:INFORMIXSERVER=informix{3}",
-                getContainerIpAddress(), String.valueOf(getMappedPort(INFORMIX_PORT)), getDatabaseName(), additionalUrlParams);
-    }
+  @Override
+  public String getDatabaseName() {
+    return databaseName;
+  }
 
-    @Override
-    public String getUsername() {
-        return DEFAULT_USER;
-    }
+  @Override
+  public String getDriverClassName() {
+    return "com.informix.jdbc.IfxDriver";
+  }
 
-    @Override
-    public String getPassword() {
-        return DEFAULT_PASSWORD;
-    }
+  @Override
+  public String getJdbcUrl() {
+    final String additionalUrlParams = constructUrlParameters(";", ";");
+    return MessageFormat.format(
+        "jdbc:informix-sqli://{0}:{1}/{2}:INFORMIXSERVER=informix{3}",
+        getContainerIpAddress(),
+        String.valueOf(getMappedPort(INFORMIX_PORT)),
+        getDatabaseName(),
+        additionalUrlParams);
+  }
 
-    @Override
-    protected String getTestQueryString() {
-        return "select count(*) from systables";
-    }
+  @Override
+  public String getPassword() {
+    return DEFAULT_PASSWORD;
+  }
 
-    @Override
-    protected void waitUntilContainerStarted() {
-        getWaitStrategy().waitUntilReady(this);
-    }
+  @Override
+  public String getUsername() {
+    return DEFAULT_USER;
+  }
 
-    @Override
-    public SELF withDatabaseName(final String databaseName) {
-        this.databaseName = databaseName;
-        return self();
-    }
+  @Override
+  public SELF withDatabaseName(final String databaseName) {
+    this.databaseName = databaseName;
+    return self();
+  }
 
-    @Override
-    public String getDatabaseName() {
-        return databaseName;
-    }
+  public SELF withInitFile(final MountableFile mountableFile) {
+    setEnvAndCopyFile(mountableFile, FileType.INIT_FILE);
+    return self();
+  }
 
-    public SELF withInitFile(MountableFile mountableFile) {
-        setEnvAndCopyFile(mountableFile, FileType.INIT_FILE);
-        return self();
-    }
+  public SELF withPostInitFile(final MountableFile mountableFile) {
+    setEnvAndCopyFile(mountableFile, FileType.RUN_FILE_POST_INIT);
+    return self();
+  }
 
-    public SELF withPostInitFile(MountableFile mountableFile) {
-        setEnvAndCopyFile(mountableFile, FileType.RUN_FILE_POST_INIT);
-        return self();
-    }
+  @Override
+  protected void configure() {
+    addEnv("LICENSE", "accept");
+  }
 
-    private void setEnvAndCopyFile(MountableFile mountableFile, FileType fileType) {
-        addEnv(fileType.toString(), Path.of(mountableFile.getFilesystemPath()).getFileName().toString());
-        withCopyFileToContainer(mountableFile, IFX_CONFIG_DIR);
-    }
+  @Override
+  protected String getTestQueryString() {
+    return "select count(*) from systables";
+  }
 
-    private enum FileType {
-        INIT_FILE, RUN_FILE_POST_INIT
-    }
+  @Override
+  protected void waitUntilContainerStarted() {
+    getWaitStrategy().waitUntilReady(this);
+  }
 
+  private void setEnvAndCopyFile(final MountableFile mountableFile, final FileType fileType) {
+    addEnv(
+        fileType.toString(), Paths.get(mountableFile.getFilesystemPath()).getFileName().toString());
+    withCopyFileToContainer(mountableFile, IFX_CONFIG_DIR);
+  }
 }
